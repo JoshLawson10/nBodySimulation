c58482d30e2de412d055ca5a0c8c547544ea9aa3
Josh Lawson <joshlawson100@outlook.com>
2026-05-11 14:41:44 +1000

    fix(bodies): label bodies with a uuid

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import random
-import uuid
 from dataclasses import dataclass
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-N_BODIES = 5
+N_BODIES = 10
 
 
 @dataclass
@@ -23,6 +22,7 @@ class Vector3:
 
 @dataclass
 class Body:
+    id: int
     mass: float
     position: Vector3
     velocity: Vector3
@@ -30,10 +30,8 @@ class Body:
     colour: str = "red"
 
     def __post_init__(self):
-        self.uuid = uuid.uuid4().int
-
         if not self.name:
-            self.name = f"Body-{self.uuid}"
+            self.name = f"Body-{self.id}"
 
     def __repr__(self) -> str:
         return (
@@ -56,7 +54,7 @@ for i in range(N_BODIES):
         random.uniform(-1e4, 1e4),
         random.uniform(-1e4, 1e4),
     )
-    bodies.append(Body(mass=mass, position=position, velocity=velocity))
+    bodies.append(Body(id=i, mass=mass, position=position, velocity=velocity))
 
 ax1 = plt.figure().add_subplot(projection="3d")
 ax1.set_title("3D Trajectories")
@@ -65,12 +63,21 @@ ax1.set_ylabel("y (m)")
 ax1.set_zlabel("z (m)")
 ax1.legend(fontsize=7)
 
-ax1.scatter3D(
-    [b.position.x for b in bodies],
-    [b.position.y for b in bodies],
-    [b.position.z for b in bodies],
-    color=[b.colour for b in bodies],
-    marker="o",
-    s=50,
-)
+for body in bodies:
+    ax1.scatter(
+        body.position.x,
+        body.position.y,
+        body.position.z,
+        label=body.name,
+        color=body.colour,
+    )
+
+    ax1.text(
+        body.position.x,
+        body.position.y,
+        body.position.z,
+        body.name,
+        fontsize=8,
+        color=body.colour,
+    )
 plt.show()
